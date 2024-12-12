91886b50eb32d3faf7a8d90a71cea52ae2e3f9f4
SucreCC <Sucre1136@gmail.com>
2024-12-12 15:15:23 +0800

    添加新的二分类数据集

---
+++ BERT-Chinese-Binary-Classification/data_loader.py
@@ -0,0 +1,13 @@
+import torch
+from tqdm import tqdm
+import time
+from datetime import timedelta
+import pandas as pd
+
+
+PAD, CLS = '[PAD]', '[CLS]'
+
+
+def load_dataset(config):
+    pd.read_csv()
+
